06fa56a733e5944a37afd1f86f5db44339d873b7
Chad Towle <towlebooth@users.noreply.github.com>
2014-11-11 10:40:14 -0600

    Update section-1.swift

--- IOS_OAuth2.playground/section-1.swift
+++ IOS_OAuth2.playground/section-1.swift
@@ -4,7 +4,7 @@ import XCPlayground
 XCPSetExecutionShouldContinueIndefinitely()
 
 // replace with your access token from https://developers.google.com/oauthplayground/
-let token = "ya29.ugBEHA3Z7ZMl_Lj9tVRyr2M2iKE8chR6y5Bpi2Dbi6PGKKErmG--Ce83OcaL8NmCAF_SOXrwrwA4vQ"
+let token = "ya29.ugBEHA3Z7ZMl_Lj9tVRyr2M2iKE8chR6y5Bpi2Dbi6PGKKErmG--Ce83OcaL8NmCAF_SOXrwrwA4wK"
 
 // replace if you are not running at the local default location
 let apiUrl = "http://127.0.0.1:3000/users/me?access_token="
